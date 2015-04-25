f53cebe16d52cb315387477c4fbaa187b5943e88
Yulia Tenincheva <yulia.ten@dezigner.ru>
2015-04-25 22:56:05 +0300

    Static cubes with color, lights and camera

--- graphics/js/theme-scene.js
+++ graphics/js/theme-scene.js
@@ -2,92 +2,116 @@ function init() {
     'use strict';
 
     var container, stats;
-    var camera, controls, scene, renderer;
-    var cross, light;
+    var camera, cameraControls, controls, scene, renderer, cubesCoords;
+    var cross, yellowLight, greyLight, object, loader, beatIt;
+
+    var clock = new THREE.Clock();
+    var chance = new Chance();
 
     init();
-    animate();
+    animateCamera();
 
     function init() {
-        camera = new THREE.PerspectiveCamera(70, window.innerWidth / window.innerHeight, 1, 1000);
+        camera = new THREE.PerspectiveCamera(90, window.innerWidth / window.innerHeight, 1, 1000);
 
-        // Set initial pozitions
+        // Set initial positions
         camera.position.x = 10;
-        camera.position.z = 150;
-        camera.position.y = 15;
+        camera.position.z = 170;
+        camera.position.y = 20;
 
-        controls = new THREE.TrackballControls(camera);
+        loader = new THREE.BinaryLoader(true);
 
-        controls.rotateSpeed = 1.0;
-        controls.zoomSpeed = 1.2;
-        controls.panSpeed = 0.8;
+        // Set camera controls
+        cameraControls = new THREE.TrackballControls(camera);
 
-        controls.noZoom = false;
-        controls.noPan = false;
+        cameraControls.rotateSpeed = 1.0;
+        cameraControls.zoomSpeed = 1.2;
+        cameraControls.panSpeed = 0.8;
 
-        controls.staticMoving = true;
-        controls.dynamicDampingFactor = 0.3;
+        cameraControls.noZoom = false;
+        cameraControls.noPan = false;
 
-        controls.keys = [65, 83, 68];
+        cameraControls.staticMoving = true;
+        cameraControls.dynamicDampingFactor = 0.3;
 
-        controls.addEventListener('change', render);
+        cameraControls.keys = [65, 83, 68];
+
+        cameraControls.addEventListener('change', render);
 
         // world
         scene = new THREE.Scene();
-        scene.fog = new THREE.FogExp2(0x000000, 0.002);
+        scene.fog = new THREE.FogExp2(0x000000, 0.003);
 
         // create the ground plane
-        var planeGeometry = new THREE.PlaneGeometry(window.innerHeight, window.innerWidth);
-        var planeMaterial = new THREE.MeshPhongMaterial({color: 0xcccccc, shading: THREE.FlatShading});
+        var planeGeometry = new THREE.CircleGeometry(700, 64);
+        var planeMaterial = new THREE.MeshLambertMaterial({color: 0x161616, shading: THREE.FlatShading});
         var plane = new THREE.Mesh(planeGeometry, planeMaterial);
 
         // rotate and position the plane
         plane.rotation.x = -0.5 * Math.PI;
-        plane.position.x = 15;
         plane.position.y = 0;
-        plane.position.z = 0;
+        plane.position.z = -20;
 
         // add the plane to the scene
         scene.add(plane);
 
-        // create a cube
-        var cubeGeometry = new THREE.BoxGeometry(10, 10, 10);
-        var cubeMaterial = new THREE.MeshBasicMaterial({color: 0xff0000, wireframe: true});
+        // Coordinates for letters
+        cubesCoords = [
+            // H
+            [-180, 5, 0], [-180, 15, 0], [-180, 25, 0], [-180, 35, 0], [-180, 45, 0], [-170, 25, 0], [-160, 5, 0], [-160, 15, 0], [-160, 25, 0], [-160, 35, 0], [-160, 45, 0],
+            // a
+            [-140, 5, 0], [-140, 15, 0], [-140, 35, 0], [-130, 15, 0], [-130, 35, 0], [-120, 5, 0], [-120, 15, 0], [-120, 25, 0], [-120, 35, 0],
+            // c
+            [-100, 5, 0], [-100, 15, 0], [-100, 25, 0], [-100, 35, 0], [-90, 5, 0], [-90, 35, 0], [-80, 5, 0], [-80, 25, 0], [-80, 35, 0],
+            // k
+            [-60, 5, 0], [-60, 15, 0], [-60, 25, 0], [-60, 35, 0], [-60, 45, 0], [-50, 15, 0], [-50, 25, 0], [-40, 5, 0], [-40, 35, 0],
 
-        var cubesH = [
-            [-80, 5, 0],
-            [-80, 15, 0],
-            [-80, 25, 0],
-            [-80, 35, 0],
-            [-80, 45, 0],
-
-            [-70, 25, 0],
-
-            [-60, 5, 0],
-            [-60, 15, 0],
-            [-60, 25, 0],
-            [-60, 35, 0],
-            [-60, 45, 0]
+            // F
+            [-20, 5, 0], [-20, 15, 0], [-20, 25, 0], [-20, 35, 0], [-20, 45, 0], [-10, 25, 0], [-10, 45, 0], [0, 25, 0], [0, 45, 0],
+            // M
+            [20, 5, 0], [20, 15, 0], [20, 25, 0], [20, 35, 0], [20, 45, 0], [30, 45, 0], [40, 5, 0], [40, 15, 0], [40, 25, 0], [40, 35, 0], [40, 45, 0], [50, 45, 0], [60, 5, 0], [60, 15, 0], [60, 25, 0], [60, 35, 0], [60, 45, 0],
+            // I
+            [80, 5, 0], [80, 45, 0], [90, 5, 0], [90, 15, 0], [90, 25, 0], [90, 35, 0], [90, 45, 0], [100, 5, 0], [100, 45, 0]
         ];
 
-        cubesH.forEach(function (coords) {
+        // create a cube
+        var cubeGeometry = new THREE.BoxGeometry(10, 10, 10);
+        var cubeMaterial;
+
+        //var selectedObject = scene.getObjectByName(object.cube);
+        //scene.remove( selectedObject );
+
+        cubesCoords.forEach(function (coords) {
+            if (coords[0] > -30) {
+                cubeMaterial = new THREE.MeshLambertMaterial({color: 0xCC9900});
+            } else {
+                cubeMaterial = new THREE.MeshLambertMaterial({color: 0xEEEEEE});
+            }
+
             var cube = new THREE.Mesh(cubeGeometry, cubeMaterial);
             cube.castShadow = true;
 
             // position the cubes
-            cube.position.x = coords[0];
-            cube.position.y = coords[1];
-            cube.position.z = coords[2];
+            cube.position.x = coords[0] + 0;
+            cube.position.y = coords[1] + 0;
+            cube.position.z = coords[2] + 0;
 
             // add the cubes to the scene
             scene.add(cube);
         });
 
-        // add spotlight for the shadows
-        var spotLight = new THREE.SpotLight(0xffffff);
-        spotLight.position.set(-90, 100, -10);
+        // white spotlight shining from the side, casting shadow
+        var spotLight = new THREE.SpotLight( 0xffffff, 2);
+        spotLight.position.set(130, 100, 100);
+
         spotLight.castShadow = true;
-        scene.add(spotLight);
+        spotLight.shadowDarkness = 1;
+
+        scene.add( spotLight );
+
+        // Additional lights
+        var light = new THREE.HemisphereLight(0xffffff, 0x000000, 1);
+        scene.add(light);
 
         // renderer
         renderer = new THREE.WebGLRenderer({antialias: false});
@@ -101,6 +125,8 @@ function init() {
         stats.domElement.style.position = 'absolute';
         stats.domElement.style.top = '0px';
         stats.domElement.style.zIndex = 100;
+
+        container.appendChild(loader.statusDomElement);
         container.appendChild(stats.domElement);
 
         window.addEventListener('resize', onWindowResize, false);
@@ -116,9 +142,9 @@ function init() {
         render();
     }
 
-    function animate() {
-        requestAnimationFrame(animate);
-        controls.update();
+    function animateCamera() {
+        requestAnimationFrame(animateCamera);
+        cameraControls.update();
     }
 
     function render() {
